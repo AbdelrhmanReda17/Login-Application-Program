2cbb77f51e86ef1ae312ca586661539a7bccad9c
Abdelrhman R,Muhammed <abdelrhmanrmuhammed@gmail.com>
2022-05-27 16:58:22 -0700

    A4

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
-/*
-FCAI – Programming 1 – 2022 - Assignment 4
-Program Name: CS112-203-2nd–S1,2-20210255-20210376-20210436-A4-LoginApp.cpp
-Program Description: Login Application
-Last Modification Date: 12 / 5 / 2022.
-Author1 and ID and Group: Abdelrhman Reda Mohamed- 20210255- GROUP A
-Author2 and ID and Group: Mahmoud Mamdouh Abdelaziz-20210376- GROUP A
-Author3 and ID and Group: Nourhan Elsayed Hassan Ibrahim-20210436- GROUP A
-Teaching Assistant: Eng. Nesma & Eng. Yousra
-Purpose: Registeration and login of users' accounts
- */
-
-
-//--------------------------------------------------
-//             int Main Function
-//--------------------------------------------------
 #include "LoginApp.h"
 #include "LoginApp.cpp"
+
 int main()
 {
     string choosing;
